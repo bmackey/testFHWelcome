10ef1fe7373f3b4283c4b03413b9f9cdf34f9c33
Brid Mackey <brid.mackey@gmail.com>
2014-01-13 22:42:59 +0000

    banner on location page

--- client/app/scripts/models/page.js
+++ client/app/scripts/models/page.js
@@ -57,13 +57,13 @@ App.models.weatherPage = new App.Model.PageModel({
   geoLabelText: 'My Location',
   getWeatherText: 'Now we are going to use that location to request weather information from an online web-service via our cloud code. Tap the button to do this.',
   getWeatherButtonText: 'Get My Weather Info',
-  titleWidth: '180'
+  titleWidth: '150'
 });
 
 App.models.integrationPage = new App.Model.PageModel({
   title: 'Cloud Integration',
   header: 'Cloud Integration',
-  titleWidth: '150',
+  titleWidth: '121',
   text: 'You can easily integrate with all of your existing back-end systems using Cloud Plugins. Check out the the App Studio to see how to connect to Databases, Messaging, Analytics, SaaS, eCommerce, Logging, Data Storage, File Storage, Testing, Tools and File Storage. A few examples including:',
   plugins:[{img:'mysql.png'},
           {img: 'oracle.png'},
